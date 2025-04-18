e0ff7f126725123d201db1367f9e0b5d1fe072cf
junforever <dgalvaro@hotmail.com>
2025-04-17 22:00:38 -0500

    fix: style changed

--- src/nodes/Argon2/Argon2Encrypt.node.ts
+++ src/nodes/Argon2/Argon2Encrypt.node.ts
@@ -1,47 +1,54 @@
-import { INodeType, INodeTypeDescription, IExecuteFunctions, INodeExecutionData } from 'n8n-workflow';
+import {
+  INodeType,
+  INodeTypeDescription,
+  IExecuteFunctions,
+  INodeExecutionData,
+} from 'n8n-workflow';
 import { NodeOperationError } from 'n8n-workflow';
 import argon2 from 'argon2';
 
 export class Argon2Encrypt implements INodeType {
-	description: INodeTypeDescription = {
-		displayName: 'Argon2 Encrypt',
-		name: 'argon2Encrypt',
-		icon: 'fa:lock',
-		group: ['transform'],
-		version: 1,
-		description: 'Encrypt (hash) text using Argon2',
-		defaults: {
-			name: 'Argon2 Encrypt',
-			color: '#00aaff',
-		},
-		inputs: ['main'] as unknown as any,
-		outputs: ['main'] as unknown as any,
-		credentials: [],
-		properties: [
-			{
-				displayName: 'Text',
-				name: 'text',
-				type: 'string',
-				required: true,
-				default: '',
-				description: 'Text to encrypt (hash)'
-			}
-		],
-	};
+  description: INodeTypeDescription = {
+    displayName: 'Argon2 Encrypt',
+    name: 'argon2Encrypt',
+    icon: 'fa:lock',
+    group: ['transform'],
+    version: 1,
+    description: 'Encrypt (hash) text using Argon2',
+    defaults: {
+      name: 'Argon2 Encrypt',
+      color: '#1E3050',
+    },
+    inputs: ['main'] as unknown as any,
+    outputs: ['main'] as unknown as any,
+    credentials: [],
+    properties: [
+      {
+        displayName: 'Text',
+        name: 'text',
+        type: 'string',
+        required: true,
+        default: '',
+        description: 'Text to encrypt (hash)',
+      },
+    ],
+  };
 
-	async execute(this: IExecuteFunctions) {
-		const items = this.getInputData();
-		const returnData: INodeExecutionData[] = [];
+  async execute(this: IExecuteFunctions) {
+    const items = this.getInputData();
+    const returnData: INodeExecutionData[] = [];
 
-		for (let i = 0; i < items.length; i++) {
-			const text = this.getNodeParameter('text', i) as string;
-			try {
-				const hash = await argon2.hash(text, { type: argon2.argon2id });
-				returnData.push({ json: { hash } });
-			} catch (error) {
-				throw new NodeOperationError(this.getNode(), error as Error, { itemIndex: i });
-			}
-		}
-		return [returnData];
-	}
+    for (let i = 0; i < items.length; i++) {
+      const text = this.getNodeParameter('text', i) as string;
+      try {
+        const hash = await argon2.hash(text, { type: argon2.argon2id });
+        returnData.push({ json: { hash } });
+      } catch (error) {
+        throw new NodeOperationError(this.getNode(), error as Error, {
+          itemIndex: i,
+        });
+      }
+    }
+    return [returnData];
+  }
 }
--- src/nodes/Argon2/Argon2Verify.node.ts
+++ src/nodes/Argon2/Argon2Verify.node.ts
@@ -1,65 +1,69 @@
-import { INodeType, INodeTypeDescription, IExecuteFunctions, INodeExecutionData } from 'n8n-workflow';
-import { NodeOperationError } from 'n8n-workflow';
+import {
+  INodeType,
+  INodeTypeDescription,
+  IExecuteFunctions,
+  INodeExecutionData,
+} from 'n8n-workflow';
 import argon2 from 'argon2';
 
 export class Argon2Verify implements INodeType {
-	description: INodeTypeDescription = {
-		displayName: 'Argon2 Verify',
-		name: 'argon2Verify',
-		icon: 'fa:lock',
-		group: ['transform'],
-		version: 1,
-		description: 'Verify text against Argon2 hash',
-		defaults: {
-			name: 'Argon2 Verify',
-			color: '#00aaff',
-		},
-		inputs: ['main'] as unknown as any,
-		outputs: ['true', 'false'] as unknown as any,
-		outputNames: ['true', 'false'],
-		credentials: [],
-		properties: [
-			{
-				displayName: 'Text',
-				name: 'text',
-				type: 'string',
-				required: true,
-				default: '',
-				description: 'Text to verify'
-			},
-			{
-				displayName: 'Hash',
-				name: 'hash',
-				type: 'string',
-				required: true,
-				default: '',
-				description: 'Hash to verify against'
-			}
-		],
-	};
+  description: INodeTypeDescription = {
+    displayName: 'Argon2 Verify',
+    name: 'argon2Verify',
+    icon: 'fa:lock-open',
+    group: ['flow'],
+    version: 1,
+    description: 'Verify text against Argon2 hash',
+    defaults: {
+      name: 'Argon2 Verify',
+      color: '#1E3050',
+    },
+    inputs: ['main'] as unknown as any,
+    outputs: ['true', 'false'] as unknown as any,
+    outputNames: ['true', 'false'],
+    credentials: [],
+    properties: [
+      {
+        displayName: 'Text',
+        name: 'text',
+        type: 'string',
+        required: true,
+        default: '',
+        description: 'Text to verify',
+      },
+      {
+        displayName: 'Hash',
+        name: 'hash',
+        type: 'string',
+        required: true,
+        default: '',
+        description: 'Hash to verify against',
+      },
+    ],
+  };
 
-	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		const items = this.getInputData();
-		const outputData: INodeExecutionData[][] = [[], []]; // true, false
+  async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+    const items = this.getInputData();
+    const outputData: INodeExecutionData[][] = [[], []]; // true, false
 
-		for (let i = 0; i < items.length; i++) {
-			const text = this.getNodeParameter('text', i) as string;
-			const hash = this.getNodeParameter('hash', i) as string;
-			try {
-				const match = await argon2.verify(hash, text);
-				if (match) {
-					outputData[0].push({ json: { match: true } });
-				} else {
-					outputData[1].push({ json: { match: false } });
-				}
-			} catch (error) {
-				let errorMessage = 'Unknown error';
-				if (error instanceof Error) {
-					errorMessage = error.message;
-				}
-				outputData[1].push({ json: { match: false, error: errorMessage } });
-			}
-		}
-		return outputData;
-	}
+    for (let i = 0; i < items.length; i++) {
+      const text = this.getNodeParameter('text', i) as string;
+      const hash = this.getNodeParameter('hash', i) as string;
+      try {
+        const match = await argon2.verify(hash, text);
+        if (match) {
+          outputData[0].push({ json: { match: true } });
+        } else {
+          outputData[1].push({ json: { match: false } });
+        }
+      } catch (error) {
+        let errorMessage = 'Unknown error';
+        if (error instanceof Error) {
+          errorMessage = error.message;
+        }
+        outputData[1].push({ json: { match: false, error: errorMessage } });
+      }
+    }
+    return outputData;
+  }
 }
